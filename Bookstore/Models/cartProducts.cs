b4dac70bb75d034f1e2717ab731416c62f0174df
Mohamed Adel Mostafa <82514591+mohamed-adel48@users.noreply.github.com>
2024-07-05 03:38:34 +0300

    Add files via upload

---
+++ Bookstore/Models/cartProducts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class cartProducts
+    {
+        public Book book { get; set; }
+        public int quantity { get; set; }
+    }
+}
